60b216655f4f382f77a2e40cac1ffd58c61e9c65
Imad Chatraoui <imadchatraoui@gmail.com>
2026-01-01 00:39:10 +0100

    Continuing 19th lesson 'scroll based animation'

--- sections/19-scroll-based-animation/src/script.js
+++ sections/19-scroll-based-animation/src/script.js
@@ -106,10 +106,13 @@ window.addEventListener('resize', () =>
 /**
  * Camera
  */
+// Group
+const cameraGroup = new THREE.Group()
+scene.add(cameraGroup)
 // Base camera
 const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 6
-scene.add(camera)
+cameraGroup.add(camera)
 
 /**
  * Renderer
@@ -132,17 +135,37 @@ window.addEventListener ('scroll', () =>{
 
     
 })
+
+/**
+ * Cursor
+ */
+
+const cursor = {}
+cursor.x = 0
+cursor.y = 0
+
+window.addEventListener('mousemove', (event) =>
+{
+    cursor.x = event.clientX / sizes.width - 0.5  // in questo modo abbiamo valori uguale per ogni dispositivo e non in base alla risoluzione di ogni client
+    cursor.y = event.clientY / sizes.height - 0.5
+    // console.log(cursor)
+})
 /**
  * Animate
  */
 const clock = new THREE.Clock()
-
+let previousTime = 0
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-
+    const deltaTime = elapsedTime - previousTime
     // Animate camera
     camera.position.y = -scrollY / sizes.height * objectDistance
+    const parallaxX = cursor.x
+    const parallaxY = - cursor.y
+    cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 0.01
+    cameraGroup.position.y += (parallaxY - cameraGroup.position.y) *0.01
+
     // Animate meshes
     for(const mesh of sectionMeshes){
         mesh.rotation.x = elapsedTime * 0.1
